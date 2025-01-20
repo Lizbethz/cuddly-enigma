a984fa6cfba5112edfbee1725bc10473decb7c64
Lizbethz <lizperezteamo@gmail.com>
2025-01-20 08:56:42 -0600

    Update descargas-instagram.js

--- plugins/descargas-instagram.js
+++ plugins/descargas-instagram.js
@@ -1,18 +1,22 @@
 import { igdl } from "ruhend-scraper";
+import fs from 'fs';
 
 const handler = async (m, { args, conn }) => {
   if (!args[0]) {
     return conn.reply(m.chat, '🔗 *Ingresa un link de Instagram*', m);
   }
-
   try {
     await conn.reply(m.chat, `🕒 *Enviando El Video...*`, m);
     let res = await igdl(args[0]);
     let data = res.data;
-
     for (let media of data) {
+      const tmpDir = './tmp';
+      if (!fs.existsSync(tmpDir)) {
+        fs.mkdirSync(tmpDir);
+      }
+      const filePath = `${tmpDir}/${media.url.split('/').pop()}`;
       await new Promise(resolve => setTimeout(resolve, 2000));
-      await conn.sendFile(m.chat, media.url, 'instagram.mp4', '🎞️ *Tu video de instagram*', m);
+      await conn.sendFile(m.chat, filePath, 'instagram.mp4', '🎞️ *Tu video de instagram*', m);
     }
   } catch (e) {
     conn.reply(m.chat, '⚙️ Ocurrió un error: ' + e, m);
